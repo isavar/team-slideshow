a08151844a64b5df4cdd295729f0dff8b79ec5b7
isavar <isabel.varela-grundmann@sinnerschrader.com>
2012-12-18 17:10:34 +0100

    Code zusammengefasst und kommentiert

--- extensions/js/slideshow.js
+++ extensions/js/slideshow.js
@@ -1,77 +1,54 @@
-	var Slideshow = function($node) {
+var Slideshow = function($node) {
 
-		var $items = $node.find(".js_slide > li"),
-			active = "active",
-			last = $items.length - 1,
-			activeIndex = 0,
-			delay = 10000,
-			interval;
+	var $items = $node.find(".js_slide > li"),
+		active = "active",
+		last = $items.length - 1,
+		activeIndex = 0,
+		delay = 10000,
+		interval;
 
-		var show = function(idx) {
-			$items.eq(activeIndex).removeClass(active);
-			activeIndex = idx;
-			$items.eq(activeIndex).addClass(active);
-		};
-
-		$node.on("click", ".js_slidepager > li", function(e) {
-			/*
-				Es gibt nur zwei LIs im pager, daher ist $(this).index() entweder
-				 0 oder 1 und kann für die Unterscheidung von vor und zurück 
-				 als bool verwendet werden.
-			*/
-			var idx = $(this).index() ? activeIndex + 1 : activeIndex - 1;
-			show(idx < 0 ? last : (idx > last ? 0 : idx));
-		});
-
-
-
-		/*
-			Beginne automatisches Durchsliden, 
-			sofern nicht mit den Bühnen interagiert wird
-		*/
-		var timer = function() {
-			interval = setInterval(function() {
-				var idx = activeIndex + 1;
-				show(idx > last ? 0 : idx);
-			}, delay);
-		};
-
-		timer();
-		$node.on("mouseenter mouseleave", function(e) {
-			clearInterval(interval);
-			if (e.type == "mouseleave") {
-				timer();
-			}
-		});
-
+	var show = function(idx) {
+		$items.eq(activeIndex).removeClass(active);
+		activeIndex = idx;
+		$items.eq(activeIndex).addClass(active);
 	};
 
-	$(document).ready(function(e) {
-		$(".js_slideshow").each(function() {
-			new Slideshow($(this));
-		});
+	$node.on("click", ".js_slidepager > li", function(e) {
+		/*
+			Es gibt nur zwei LIs im pager, daher ist $(this).index() entweder
+			 0 oder 1 und kann für die Unterscheidung von vor und zurück 
+			 als bool verwendet werden.
+		*/
+		var idx = $(this).index() ? activeIndex + 1 : activeIndex - 1;
+		show(idx < 0 ? last : (idx > last ? 0 : idx));
 	});
 
 
 
+	/*
+		Beginne automatisches Durchsliden, 
+		sofern nicht mit den Bühnen interagiert wird
+	*/
+	var timer = function() {
+		interval = setInterval(function() {
+			var idx = activeIndex + 1;
+			show(idx > last ? 0 : idx);
+		}, delay);
+	};
 
-/** Modernizr Tests ************************************************/
+	timer();
+	$node.on("mouseenter mouseleave", function(e) {
+		clearInterval(interval);
+		if (e.type == "mouseleave") {
+			timer();
+		}
+	});
 
-// developer.mozilla.org/en/CSS/pointer-events
-// github.com/ausi/Feature-detection-technique-for-pointer-events
-Modernizr.addTest('pointerevents', function(){
-	var element = document.createElement('div'),
-			documentElement = document.documentElement,
-			getComputedStyle = window.getComputedStyle,
-			supports;
-	if(!('pointerEvents' in element.style)){
-			return false;
-	}
-	element.style.pointerEvents = 'auto';
-	element.style.pointerEvents = 'x';
-	documentElement.appendChild(element);
-	supports = getComputedStyle &&
-			getComputedStyle(element, '').pointerEvents === 'auto';
-	documentElement.removeChild(element);
-	return !!supports;
+};
+
+$(document).ready(function(e) {
+	$(".js_slideshow").each(function() {
+		new Slideshow($(this));
+	});
 });
+
