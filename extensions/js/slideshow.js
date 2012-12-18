0feb3913e4c48e055ba413d97df9636e4820aa74
isavar <isabel.varela-grundmann@sinnerschrader.com>
2012-12-18 17:01:05 +0100

    Anpassungen für IE

--- extensions/js/slideshow.js
+++ extensions/js/slideshow.js
@@ -1,12 +1,11 @@
 	var Slideshow = function($node) {
 
-		var $items = $node.find("li"),
-				$pager = $node.next(".js_slidepager"),
-				active = "active",
-				last = $items.length - 1,
-				activeIndex = 0,
-				delay = 10000,
-				interval;
+		var $items = $node.find(".js_slide > li"),
+			active = "active",
+			last = $items.length - 1,
+			activeIndex = 0,
+			delay = 10000,
+			interval;
 
 		var show = function(idx) {
 			$items.eq(activeIndex).removeClass(active);
@@ -14,26 +13,7 @@
 			$items.eq(activeIndex).addClass(active);
 		};
 
-		var timer = function() {
-			interval = setInterval(function() {
-					var idx = activeIndex + 1;
-					show(idx > last ? 0 : idx);
-			}, delay);
-		};
-
-		/*
-			Beginne automatisches Durchsliden, 
-			sofern nicht mit den Bühnen interagiert wird
-		*/
-		timer();
-		$node.on("mouseenter mouseleave", function(e) {
-			clearInterval(interval);
-			if (e.type == "mouseleave") {
-					timer();
-			}
-		});
-
-		$pager.on("click", "li", function(e) {
+		$node.on("click", ".js_slidepager > li", function(e) {
 			/*
 				Es gibt nur zwei LIs im pager, daher ist $(this).index() entweder
 				 0 oder 1 und kann für die Unterscheidung von vor und zurück 
@@ -42,10 +22,32 @@
 			var idx = $(this).index() ? activeIndex + 1 : activeIndex - 1;
 			show(idx < 0 ? last : (idx > last ? 0 : idx));
 		});
+
+
+
+		/*
+			Beginne automatisches Durchsliden, 
+			sofern nicht mit den Bühnen interagiert wird
+		*/
+		var timer = function() {
+			interval = setInterval(function() {
+				var idx = activeIndex + 1;
+				show(idx > last ? 0 : idx);
+			}, delay);
+		};
+
+		timer();
+		$node.on("mouseenter mouseleave", function(e) {
+			clearInterval(interval);
+			if (e.type == "mouseleave") {
+				timer();
+			}
+		});
+
 	};
 
 	$(document).ready(function(e) {
-		$(".js_slide").each(function() {
+		$(".js_slideshow").each(function() {
 			new Slideshow($(this));
 		});
 	});
