939c18743f073b317ecd735aa9245a6a945a9faa
isavar <isabel.varela-grundmann@sinnerschrader.com>
2013-01-23 16:48:53 +0100

    Code-Formatierung

--- extensions/js/imagescaler.js
+++ extensions/js/imagescaler.js
@@ -1,66 +1,62 @@
 var Imagescaler = function($node) {
-  var imgWidth       = 1024,  //Originalmaße der verwendeten Bilder
-      imgHeight      = 681,
-      visualratio    = imgWidth / imgHeight,
-      selector       = "img.scaleimg",
-      round          = function(i){ return (parseInt( i*10000, 10 ) / 10000); },
-      styleDoc       = (document.getElementById("js_scale_styles").sheet || document.styleSheets["js_scale_styles"]),
-      $slots         = $node.find("li"),
-      slotwidth, slotheight, slotratio, rulesStr, width, height;
+	var visualratio = 1024 / 681, //Originalmaße der verwendeten Bilder
+		selector 	= "img.scaleimg",
+		round 		= function(i){ return (parseInt( i*10000, 10 ) / 10000); },
+		styleDoc 	= (document.getElementById("js_scale_styles").sheet || document.styleSheets["js_scale_styles"]),
+		$slots 		= $node.find("li"),
+		slotwidth, slotheight, slotratio, rulesStr, width, height;
 
-  var resizeImg = function() {
-    slotwidth = $slots.first().width();
-    slotheight = $slots.first().height();
-    slotratio = slotwidth / slotheight;
-    
-    if (slotratio < visualratio) {
-      width = slotheight * visualratio;
-      height = width / visualratio;
-    } else {
-      height = slotwidth / visualratio;
-      width = height * visualratio;
-    }
+	var resizeImg = function() {
+		slotwidth = $slots.first().width();
+		slotheight = $slots.first().height();
+		slotratio = slotwidth / slotheight;
+		
+		if (slotratio < visualratio) {
+			width = slotheight * visualratio;
+			height = width / visualratio;
+		} else {
+			height = slotwidth / visualratio;
+			width = height * visualratio;
+		}
 
-    rulesStr = ["width:", round(width), "px !important;",
-                "height:", round(height), "px !important;",
-                "margin: 0 0 0 ", round( (width - slotwidth) / -2), "px;\n"].join("");
+		rulesStr = ["width:", round(width), "px !important;",
+					"height:", round(height), "px !important;",
+					"margin: 0 0 0 ", round( (width - slotwidth) / -2), "px;\n"].join("");
 
-    if (styleDoc.insertRule) {
-      styleDoc.cssRules.length && styleDoc.deleteRule(0);
-      styleDoc.insertRule(selector + "{" + rulesStr + "}", 0);
-    } else {
-      styleDoc.cssRules && styleDoc.cssRules.length && styleDoc.removeRule();
-      styleDoc.addRule(selector, rulesStr);
-    }
+		if (styleDoc.insertRule) {
+			styleDoc.cssRules.length && styleDoc.deleteRule(0);
+			styleDoc.insertRule(selector + "{" + rulesStr + "}", 0);
+		} else {
+			styleDoc.cssRules && styleDoc.cssRules.length && styleDoc.removeRule();
+			styleDoc.addRule(selector, rulesStr);
+		}
 
-    $slots.addClass("resized");
-  };
-  
-  var replaceBackgroundImage = function($elm) {
-    var imgURL = $elm.css('background-image').match(/url\("(.*)"\)/)[1];
-    var $imgElement = $("<img class='scaleimg'/>").attr('src', imgURL);
-    $elm.append($imgElement);
-    //resizeImg($elm);
-    $elm.css('background-image','none');
-  };
+		$slots.addClass("resized");
+	};
+	
+	var replaceBackgroundImage = function($elm) {
+		var imgURL = $elm.css('background-image').match(/url\("(.*)"\)/)[1];
+		var $imgElement = $("<img class='scaleimg'/>").attr('src', imgURL);
+		$elm.append($imgElement).css('background-image','none');
+	};
 
 
-  $slots.each(function(idx, elm) {
-    replaceBackgroundImage($(elm));
-  });
-  resizeImg();
-  
-  $(window).bind("resize", resizeImg);
+	$slots.each(function(idx, elm) {
+		replaceBackgroundImage($(elm));
+	});
+	resizeImg();
+	
+	$(window).bind("resize", resizeImg);
 
 };
 
 
 $(document).ready(function(e) {
-  if (Modernizr.backgroundsize) { 
-    return; // JS wird nur benötigt, wenn der Browser kein background-size kann
-  }
+	if (Modernizr.backgroundsize) { 
+		return; // JS wird nur benötigt, wenn der Browser kein background-size kann
+	}
 
-  $(".js_scale").each(function() {
-    new Imagescaler($(this));
-  });
+	$(".js_scale").each(function() {
+		new Imagescaler($(this));
+	});
 });
